8eca5b1d622d3e357f78ac98c166cdb8aa077a2c
Baibhav-gupta <baibhavgupta420@gmail.com>
2023-09-21 06:53:55 +0530

    first Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        RWOnly obj=new RWOnly();
+        System.out.println(obj.getName());
+        obj.setName("ram");
+        System.out.println(obj.getName());
+    }
   
 }
